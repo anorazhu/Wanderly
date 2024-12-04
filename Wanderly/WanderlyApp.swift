800ce05f7fc4ca9a52426a3e3f200b2103e197f4
anorazhu <zhusm@bc.edu>
2024-12-03 19:10:20 -0500

    Initial Commit

---
+++ Wanderly/WanderlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderlyApp.swift
+//  Wanderly
+//
+//  Created by Anora Zhu on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WanderlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WanderlyTests/WanderlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WanderlyTests.swift
+//  WanderlyTests
+//
+//  Created by Anora Zhu on 12/3/24.
+//
+
+import Testing
+@testable import Wanderly
+
+struct WanderlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
