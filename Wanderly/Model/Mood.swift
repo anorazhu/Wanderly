b7dceb91ba2108b33cdf3c5dddc7b111700776d8
anorazhu <zhusm@bc.edu>
2024-12-09 09:33:41 -0500

    api working and can recognize which city user inputs, then generates activity list

---
+++ Wanderly/Model/Mood.swift
@@ -0,0 +1,8 @@
+//
+//  Mood.swift
+//  Wanderly
+//
+//  Created by Anora Zhu on 12/9/24.
+//
+
+import Foundation
---
+++ Wanderly/Views/ActivityCard.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityCard.swift
+//  Wanderly
+//
+//  Created by Anora Zhu on 12/9/24.
+//
+
+import SwiftUI
+
+struct ActivityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityCard()
+}
